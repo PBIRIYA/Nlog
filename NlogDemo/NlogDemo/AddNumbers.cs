cb6e586d55c4f8962087d6a4cfdb84101604fe24
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-07 15:30:23 +0530

    [Priya] add . folder

---
+++ NlogDemo/NlogDemo/AddNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace NlogDemo
+{
+    public class AddNumbers
+    {
+        private readonly Nlog nlog = new Nlog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            ///Checking values equal to 0 
+            if (firstNumber == 0 || secondNumber == 0)
+                {
+                nlog.LogDebug("Debug Uncessfull : Sum()");
+                nlog.LogError("Input Numbers cannot be zero");
+                nlog.LogWarn("firstNumber or secondNumber should not equal to 0");
+            }
+            int result = firstNumber + secondNumber;
+            nlog.LogDebug("Debug Uncessfull : Sum()");
+            nlog.LogInfo("Sum Method passed, Result :" + result);
+            return result;
+        }
+    }
+}
---
+++ NlogDemo/NlogDemo/Nlog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NLog;
+
+namespace NlogDemo
+{
+    public class Nlog
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Fine-grained statements concerning program state, typically used for debugging,
+        /// </summary>
+        /// <param name="="message"></param>
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+        /// <summary>
+        /// Statements that describe non fatal errors in the application
+        /// this level is used quite often for logging handled exceptions
+        /// <param name="="message"></param>{
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+        /// <summary>
+        /// Statements that describe ptentially harmful events or states in the program
+        /// </summary>
+        /// <param name="="message"></param>
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+    }
+}
